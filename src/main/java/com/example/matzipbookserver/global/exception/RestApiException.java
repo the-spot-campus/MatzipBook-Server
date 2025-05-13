4aaa49c605e608a9d9202cb4e72142fbbc708d22
MongsangGa <cldnwjd1024@gmail.com>
2025-05-13 17:18:27 +0900

    feat(RestApiException): 런타임 예외 추가

---
+++ src/main/java/com/example/matzipbookserver/global/exception/RestApiException.java
@@ -0,0 +1,16 @@
+package com.example.matzipbookserver.global.exception;
+
+import com.example.matzipbookserver.global.response.error.ErrorCode;
+
+public class RestApiException extends RuntimeException {
+
+    private final ErrorCode errorCode;
+
+    public RestApiException(ErrorCode errorCode) {
+        this.errorCode = errorCode;
+    }
+
+    public ErrorCode getErrorCode() {
+        return errorCode;
+    }
+}
