d6af2e4f78b159e38fb1f0cf984eae7e351b7d20
MongsangGa <cldnwjd1024@gmail.com>
2025-05-13 17:19:34 +0900

    feat(SuccessResponse): ResponseEntity를 확장한 래퍼 구현

---
+++ src/main/java/com/example/matzipbookserver/global/response/SuccessResponse.java
@@ -0,0 +1,20 @@
+package com.example.matzipbookserver.global.response;
+
+import com.example.matzipbookserver.global.response.success.SuccessCode;
+import org.springframework.http.ResponseEntity;
+
+public class SuccessResponse<T> extends ResponseEntity {
+    public SuccessResponse(final SuccessCode successCode, final T result) {
+        super(new CustomResponseDto<>(successCode.getCode(), result),
+                successCode.getHttpStatus()
+        );
+    }
+
+    public static <T> SuccessResponse<T> of(final SuccessCode successCode, final T result) {
+        return new SuccessResponse<>(successCode, result);
+    }
+
+    public static SuccessResponse of(final SuccessCode successCode) {
+        return new SuccessResponse(successCode, successCode.getMessage());
+    }
+}
