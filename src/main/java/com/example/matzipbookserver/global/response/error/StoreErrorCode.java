15d2c2bac5f831ba7934641be31bedfb29a9331f
xxubin04 <yubinjeong40@gmail.com>
2025-06-02 19:36:54 +0900

    feat(storeErrorCode): 유효하지 않은 필터 조건 에러 코드 추가

--- src/main/java/com/example/matzipbookserver/global/response/error/StoreErrorCode.java
+++ src/main/java/com/example/matzipbookserver/global/response/error/StoreErrorCode.java
@@ -8,7 +8,9 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor
 public enum StoreErrorCode implements ErrorCode{
 
-    STORE_NOT_EXIST("PLACE_NOT_FOUND_001", HttpStatus.NOT_FOUND,"카카오에서 장소를 찾을 수 없음");
+    STORE_NOT_EXIST("PLACE_NOT_FOUND_001", HttpStatus.NOT_FOUND,"카카오에서 장소를 찾을 수 없음"),
+    STORE_FILTER_FAILED("STORE_FILTER_ERROR_001", HttpStatus.INTERNAL_SERVER_ERROR, "가게 필터링 중 오류 발생"),
+    INVALID_FILTER_CONDITION("STORE_FILTER_ERROR_002", HttpStatus.BAD_REQUEST, "잘못된 필터링 조건");
 
     private final String developCode;
     private final HttpStatus httpStatus;
