aa71cfad4fc9820b3d801511d2b2150f1b7acabe
pu2rile <3o920@naver.com>
2025-06-04 16:14:03 +0900

    feat(BookmarkService): 북마크 저장/삭제 기능 로직 구현

--- src/main/java/com/example/matzipbookserver/bookmark/service/BookmarkService.java
+++ src/main/java/com/example/matzipbookserver/bookmark/service/BookmarkService.java
@@ -2,11 +2,12 @@ package com.example.matzipbookserver.bookmark.service;
 
 import com.example.matzipbookserver.bookmark.controller.dto.request.BookmarkRequest;
 import com.example.matzipbookserver.bookmark.controller.dto.response.BookmarkInfo;
-import com.example.matzipbookserver.bookmark.domain.repository.BookmarkRepository;
 import com.example.matzipbookserver.bookmark.domain.entity.BookmarkEntity;
+import com.example.matzipbookserver.bookmark.domain.repository.BookmarkRepository;
+import com.example.matzipbookserver.bookmark.response.error.BookmarkError;
+import com.example.matzipbookserver.global.exception.RestApiException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-
 
 import java.util.List;
 
@@ -21,5 +22,28 @@ public class BookmarkService {
         return bookmarks.stream()
                 .map(bookmark -> new BookmarkInfo(bookmark.getStoreId()))
                 .toList();
+    }
+
+    public void saveBookmark(BookmarkRequest request) {
+        boolean exists = bookmarkRepository.existsByUserIdAndStoreId(request.userId(), request.storeId());
+        if (exists) {
+            throw new RestApiException(BookmarkError.ALREADY_BOOKMARKED);
+        }
+
+        BookmarkEntity bookmark = BookmarkEntity.builder()
+                .userId(request.userId())
+                .storeId(request.storeId())
+                .build();
+
+        bookmarkRepository.save(bookmark);
+    }
+
+    public void deleteBookmark(BookmarkRequest request) {
+        boolean exists = bookmarkRepository.existsByUserIdAndStoreId(request.userId(), request.storeId());
+        if (!exists) {
+            throw new RestApiException(BookmarkError.BOOKMARK_NOT_FOUND);
+        }
+
+        bookmarkRepository.deleteByUserIdAndStoreId(request.userId(), request.storeId());
     }
 }
