feda34a84a2ce41798cb094a2a2633c4df111bfa
taeyeongKims <xodud8530@gmail.com>
2025-06-04 13:30:44 +0900

    aws s3 config 설정

---
+++ src/main/java/com/example/matzipbookserver/global/config/AmazonS3Config.java
@@ -0,0 +1,29 @@
+package com.example.matzipbookserver.global.config;
+
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSCredentialsProvider;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import jakarta.annotation.PostConstruct;
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@Getter
+public class AmazonS3Config {
+    @Value("${aws.s3.bucket}")
+    private String bucket;
+    @Value("${aws.region}")
+    private String region;
+
+    @Bean
+    public AmazonS3 amazonS3() {
+        return AmazonS3ClientBuilder.standard()
+                .withRegion(region)
+                .build();
+    }
+}
