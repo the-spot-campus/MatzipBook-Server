d71a45b834c4f26c1e5598c5a5fffc22d0165234
taeyeongKims <xodud8530@gmail.com>
2025-06-05 11:18:55 +0900

    #19 s3 response code

--- src/main/java/com/example/matzipbookserver/global/response/error/S3ErrorCode.java
+++ src/main/java/com/example/matzipbookserver/global/response/error/S3ErrorCode.java
@@ -1,4 +1,18 @@
 package com.example.matzipbookserver.global.response.error;
 
-public enum S3ErrorCode {
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@RequiredArgsConstructor
+public enum S3ErrorCode implements ErrorCode {
+    POST_IMAGE_ERROR("S3_001", HttpStatus.BAD_REQUEST, "올바르지 않은 이미지 저장 접근입니다."),
+    DELETE_IMAGE_ERROR("S3_002", HttpStatus.BAD_REQUEST, "올바르지 않은 이미지 삭제 접근입니다."),
+    PROFILE_UPLOAD_FAILED("S3_003", HttpStatus.INTERNAL_SERVER_ERROR,"이미지 업로드를 실패하였습니다.");
+
+    private final String developCode;
+    private final HttpStatus httpStatus;
+    private final String message;
+
 }
--- src/main/java/com/example/matzipbookserver/global/response/success/S3SuccessCode.java
+++ src/main/java/com/example/matzipbookserver/global/response/success/S3SuccessCode.java
@@ -1,4 +1,16 @@
 package com.example.matzipbookserver.global.response.success;
 
-public enum S3SuccessCode {
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@RequiredArgsConstructor
+public enum S3SuccessCode implements SuccessCode {
+    PROFILE_UPLOAD_SUCCESS("S3_001", HttpStatus.OK, "프로필 이미지를 성공적으로 업로드하였습니다"),
+    PROFILE_DELETED_SUCCESS("S3_002", HttpStatus.OK, "프로필 이미지를 성공적으로 삭제하였습니다.");
+
+    private final String code;
+    private final HttpStatus httpStatus;
+    private final String message;
 }
