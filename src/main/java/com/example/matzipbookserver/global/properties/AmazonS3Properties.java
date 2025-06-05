203698631db7d249cb00769c0b853208486fffbb
taeyeongKims <xodud8530@gmail.com>
2025-06-05 11:18:05 +0900

    #19 s3 properties

--- src/main/java/com/example/matzipbookserver/global/properties/AmazonS3Properties.java
+++ src/main/java/com/example/matzipbookserver/global/properties/AmazonS3Properties.java
@@ -1,4 +1,18 @@
 package com.example.matzipbookserver.global.properties;
 
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+@Getter
 public class AmazonS3Properties {
+    @Value("${aws.s3.bucket}")
+    private String bucket;
+    @Value("${cloud.aws.credentials.accessKey}")
+    private String accessKey;
+    @Value("${cloud.aws.credentials.secretKey}")
+    private String secretKey;
+    @Value("${aws.region}")
+    private String region;
 }
