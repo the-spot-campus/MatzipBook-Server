0ea3c819b23cbfe35377d48e1e770b5f6b4420ce
taeyeongKims <xodud8530@gmail.com>
2025-06-05 11:17:46 +0900

    #19 aws s3 config

--- src/main/java/com/example/matzipbookserver/global/config/AmazonS3Config.java
+++ src/main/java/com/example/matzipbookserver/global/config/AmazonS3Config.java
@@ -6,8 +6,10 @@ import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.example.matzipbookserver.global.properties.AmazonS3Properties;
 import jakarta.annotation.PostConstruct;
 import lombok.Getter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -15,15 +17,16 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @Getter
 public class AmazonS3Config {
-    @Value("${aws.s3.bucket}")
-    private String bucket;
-    @Value("${aws.region}")
-    private String region;
+
+    @Autowired
+    private AmazonS3Properties amazonS3Properties;
 
     @Bean
     public AmazonS3 amazonS3() {
+        AWSCredentials awsCredentials = new BasicAWSCredentials(amazonS3Properties.getAccessKey(), amazonS3Properties.getSecretKey());
         return AmazonS3ClientBuilder.standard()
-                .withRegion(region)
+                .withRegion(amazonS3Properties.getRegion())
+                .withCredentials(new AWSStaticCredentialsProvider(awsCredentials))
                 .build();
     }
 }
