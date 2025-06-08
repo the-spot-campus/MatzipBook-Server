c55c9ce0c46ea576f5871fa51fd469f633fb04a1
taeyeongKims <xodud8530@gmail.com>
2025-06-08 15:22:07 +0900

    #19(controller) s3 controller

--- src/main/java/com/example/matzipbookserver/s3/controller/S3Controller.java
+++ src/main/java/com/example/matzipbookserver/s3/controller/S3Controller.java
@@ -1,9 +1,9 @@
-package com.example.matzipbookserver.uploader.controller;
+package com.example.matzipbookserver.s3.controller;
 
-import com.example.matzipbookserver.uploader.controller.dto.reqeust.DeleteProfileRequest;
-import com.example.matzipbookserver.uploader.controller.dto.reqeust.UploadProfileRequest;
-import com.example.matzipbookserver.uploader.controller.dto.response.UploadProfileResponse;
-import com.example.matzipbookserver.uploader.service.S3Service;
+import com.example.matzipbookserver.s3.controller.dto.reqeust.DeleteProfileRequest;
+import com.example.matzipbookserver.s3.controller.dto.reqeust.UploadProfileRequest;
+import com.example.matzipbookserver.s3.controller.dto.response.UploadProfileResponse;
+import com.example.matzipbookserver.s3.service.S3Service;
 import com.example.matzipbookserver.global.resolver.CurrentMember;
 import com.example.matzipbookserver.global.response.SuccessResponse;
 import com.example.matzipbookserver.global.response.success.S3SuccessCode;
@@ -18,19 +18,19 @@ import org.springframework.web.multipart.MultipartFile;
 public class S3Controller {
     private final S3Service S3Service;
 
-    @PostMapping(value = "/upload/profile", consumes = "multipart/form-data")
+    @PostMapping(value = "/profile", consumes = "multipart/form-data")
     public SuccessResponse<UploadProfileResponse> uploadProfileImage(
             @CurrentMember Member member,
-            @RequestParam("profileImage") MultipartFile profileImage) {
+            @RequestPart("profileImage") MultipartFile profileImage) {
         UploadProfileRequest request = new UploadProfileRequest(member, profileImage);
         return SuccessResponse.of(S3SuccessCode.PROFILE_UPLOAD_SUCCESS, S3Service.uploadProfileImage(request));
     }
 
-    @DeleteMapping("/delete/profile")
+    @DeleteMapping("/profile")
     public SuccessResponse<String> deleteProfileImage(
             @CurrentMember Member member
     ) {
         S3Service.deleteProfileImage(new DeleteProfileRequest(member));
-        return SuccessResponse.of(S3SuccessCode.PROFILE_UPLOAD_SUCCESS, "프로필 이미지를 성공적으로 삭제하였습니다.");
+        return SuccessResponse.of(S3SuccessCode.PROFILE_DELETED_SUCCESS, "프로필 이미지를 성공적으로 삭제하였습니다.");
     }
 }
