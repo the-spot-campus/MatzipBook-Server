9d379b920a3bc3901132e2692723573a852ebb49
taeyeongKims <xodud8530@gmail.com>
2025-06-08 15:24:25 +0900

    #19 (member.dto) profileImage 필드 추가

--- src/main/java/com/example/matzipbookserver/member/controller/dto/request/SignUpRequest.java
+++ src/main/java/com/example/matzipbookserver/member/controller/dto/request/SignUpRequest.java
@@ -1,12 +1,26 @@
 package com.example.matzipbookserver.member.controller.dto.request;
 
-public record SignupRequest (
+import lombok.Builder;
+
+@Builder
+public record SignUpRequest(
         String email,
         String provider,
         String providerId,
         String nickname,
         String birth,
         String gender,
-        String profileImagePath,
         String university
-) {}
+) {
+    public static SignUpRequest from(String email, String provider, String providerId, String nickname, String birth, String gender, String university){
+        return SignUpRequest.builder()
+                .email(email)
+                .provider(provider)
+                .providerId(providerId)
+                .nickname(nickname)
+                .birth(birth)
+                .gender(gender)
+                .university(university)
+                .build();
+    }
+}
