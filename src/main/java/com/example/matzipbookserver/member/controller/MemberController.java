1fdf9f55021dfb8a1206ef0df98bb7907973b1e3
taeyeongKims <xodud8530@gmail.com>
2025-06-08 15:25:13 +0900

    #19 (member.controller) profileImage 추가 및 기능별 정리

--- src/main/java/com/example/matzipbookserver/member/controller/MemberController.java
+++ src/main/java/com/example/matzipbookserver/member/controller/MemberController.java
@@ -1,4 +1,20 @@
 package com.example.matzipbookserver.member.controller;
 
+import com.example.matzipbookserver.global.resolver.CurrentMember;
+import com.example.matzipbookserver.global.response.SuccessResponse;
+import com.example.matzipbookserver.global.response.success.MemberSuccessCode;
+import com.example.matzipbookserver.member.controller.dto.response.MemberInfoResponse;
+import com.example.matzipbookserver.member.domain.Member;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/member")
 public class MemberController {
+
+    @GetMapping("/me")
+    public SuccessResponse<MemberInfoResponse> getCurrentMember(@CurrentMember Member member) {
+        return SuccessResponse.of(MemberSuccessCode.MEMBER_INFO_SUCCESS, MemberInfoResponse.from(member));
+    }
 }
