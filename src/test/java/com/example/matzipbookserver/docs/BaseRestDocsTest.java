1327e0f0f6d7ae369affed6082f4f29bcb8d5dc4
taeyeongKims <xodud8530@gmail.com>
2025-06-08 15:39:13 +0900

    #19(test.docs) restDocs 파일 모음 디렉토리

--- src/test/java/com/example/matzipbookserver/docs/BaseRestDocsTest.java
+++ src/test/java/com/example/matzipbookserver/docs/BaseRestDocsTest.java
@@ -1,12 +1,13 @@
-package com.example.matzipbookserver;
+package com.example.matzipbookserver.docs;
 
+import com.example.matzipbookserver.global.DatabaseCleanUp;
+import com.example.matzipbookserver.global.DummyGenerator;
 import com.example.matzipbookserver.bookmark.controller.BookmarkController;
 import com.example.matzipbookserver.member.controller.AuthController;
 import com.example.matzipbookserver.member.controller.MemberController;
 import com.example.matzipbookserver.member.domain.Member;
 import com.example.matzipbookserver.s3.controller.S3Controller;
 import com.example.matzipbookserver.store.controller.StoreController;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
@@ -19,7 +20,7 @@ import org.springframework.test.web.servlet.MockMvc;
 @AutoConfigureMockMvc
 @AutoConfigureRestDocs
 @SpringBootTest
-@ActiveProfiles("test")
+@ActiveProfiles({"test", "secret"})
 public abstract class BaseRestDocsTest {
 
     @SpyBean protected AuthController authController;
@@ -30,14 +31,15 @@ public abstract class BaseRestDocsTest {
 
     @Autowired protected MockMvc mockMvc;
     @Autowired protected DummyGenerator dummyGenerator;
+    @Autowired private DatabaseCleanUp databaseCleanUp;
 
     protected Member member;
     protected String GIVEN_ACCESS_TOKEN;
-    protected ObjectMapper objectMapper = new ObjectMapper();
 
     @BeforeEach
     void setUp() {
-        member = dummyGenerator.generateSingleMember();
-        GIVEN_ACCESS_TOKEN = dummyGenerator.generateAccessToken(member);
+        databaseCleanUp.execute();
+        member = dummyGenerator.createSingleMemberWithMemberImage();
+        GIVEN_ACCESS_TOKEN = dummyGenerator.createAccessToken(member);
     }
 }
--- src/test/java/com/example/matzipbookserver/docs/S3RestDocsTest.java
+++ src/test/java/com/example/matzipbookserver/docs/S3RestDocsTest.java
@@ -1,59 +1,121 @@
 package com.example.matzipbookserver.docs;
 
-import com.example.matzipbookserver.global.BaseIntegrationTest;
-import com.example.matzipbookserver.global.DummyGenerator;
+import com.example.matzipbookserver.global.exception.RestApiException;
+import com.example.matzipbookserver.global.response.CommonErrorResponseField;
+import com.example.matzipbookserver.global.response.CommonSuccessResponseField;
+import com.example.matzipbookserver.global.response.SuccessResponse;
+import com.example.matzipbookserver.global.response.error.S3ErrorCode;
+import com.example.matzipbookserver.global.response.success.S3SuccessCode;
+import com.example.matzipbookserver.s3.controller.dto.response.UploadProfileResponse;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockMultipartFile;
 
+import static org.mockito.Mockito.*;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.delete;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.multipart;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
+import static org.springframework.restdocs.request.RequestDocumentation.partWithName;
+import static org.springframework.restdocs.request.RequestDocumentation.requestParts;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-class S3ControllerTest extends BaseRestDocsTest {
-
-    @Autowired
-    private DummyGenerator dummyGenerator;
+class S3RestDocsTest extends BaseRestDocsTest {
 
     @Test
-    @DisplayName("API - 회원가입")
-    void signUp() throws Exception {
-        final SignUpRequest request = createSampleSignUpRequest();
+    @DisplayName("s3 프로필 이미지 업로드")
+    void signUpSuccess() throws Exception {
+        // given
+        MockMultipartFile multipartFile = dummyGenerator.createMockMultipartFile();
+        final SuccessResponse<?> response = SuccessResponse.of(
+                S3SuccessCode.PROFILE_UPLOAD_SUCCESS, new UploadProfileResponse("test_url"));
 
-        MockMultipartFile multipartFile = createMultipartFile();
-        MockMultipartFile requestMultipartFile = new MockMultipartFile("request",
-                null, "application/json", objectMapper.writeValueAsString(request).getBytes(StandardCharsets.UTF_8));
-
-        dummyGenerator.authenticateEmail(request.email());
+        doReturn(response).when(s3Controller).uploadProfileImage(any(), any());
 
         // When and Then
-        mockMvc.perform(multipart("/members")
+        mockMvc.perform(multipart("/api/profile")
                         .file(multipartFile)
-                        .file(requestMultipartFile)
+                        .header("Authorization", GIVEN_ACCESS_TOKEN)
                         .contentType(MediaType.MULTIPART_FORM_DATA)
                 )
-                .andExpect(status().is2xxSuccessful())
-                .andDo(document("member-signup",
+                .andExpect(status().isOk())
+                .andDo(document("profile/upload",
                         requestParts(
-                                partWithName("profileImage").description("프로필 이미지 파일"),
-                                partWithName("request").description("회원가입 정보")
-                        ),
-                        requestPartFields("request",
-                                fieldWithPath("name").description("이름"),
-                                fieldWithPath("email").description("이메일"),
-                                fieldWithPath("password").description("비밀번호"),
-                                fieldWithPath("mbti").description("MBTI ex) INTJ"),
-                                fieldWithPath("gender").description("성별 ex) MALE, FEMALE"),
-                                fieldWithPath("nationality").description("국적"),
-                                fieldWithPath("birthday").description("생년월일"),
-                                fieldWithPath("aboutMe").description("자기소개")
+                                partWithName("profileImage").description("프로필 이미지 파일")
                         ),
                         responseFields(
-                                fieldWithPath("code").description("성공 코드"),
-                                fieldWithPath("result").description("응답 결과")
+                                CommonSuccessResponseField.createCommonResponseFields()
+                        ).andWithPrefix("result.",
+                                fieldWithPath("path").description("이미지 경로"))
+                ));
+    }
+
+    @Test
+    @DisplayName("s3 프로필 이미지 업로드 실패")
+    void signUpFail() throws Exception {
+        // given
+        MockMultipartFile multipartFile = dummyGenerator.createMockMultipartFile();
+        doThrow(new RestApiException(S3ErrorCode.PROFILE_UPLOAD_FAILED))
+                .when(s3Controller).uploadProfileImage(any(), any());
+
+        // When and Then
+        mockMvc.perform(multipart("/api/profile")
+                        .file(multipartFile)
+                        .header("Authorization", GIVEN_ACCESS_TOKEN)
+                        .contentType(MediaType.MULTIPART_FORM_DATA)
+                )
+                .andExpect(status().isInternalServerError())
+                .andDo(document("profile/upload-fail",
+                        requestParts(
+                                partWithName("profileImage").description("프로필 이미지 파일")
+                        ),
+                        responseFields(
+                                CommonErrorResponseField.createCommonErrorResponseFields()
                         )
                 ));
     }
 
+    @Test
+    @DisplayName("s3 프로필 이미지 삭제 성공")
+    void deleteProfileImageSuccess() throws Exception {
+        // given
+        final SuccessResponse<String> response = SuccessResponse.of(
+                S3SuccessCode.PROFILE_DELETED_SUCCESS, "프로필 이미지를 성공적으로 삭제하였습니다.");
 
+        doReturn(response).when(s3Controller).deleteProfileImage(any());
+
+        // When and Then
+        mockMvc.perform(delete("/api/profile")
+                        .header("Authorization", GIVEN_ACCESS_TOKEN)
+                        .contentType(MediaType.APPLICATION_JSON)
+                )
+                .andExpect(status().isOk())
+                .andDo(document("profile/delete",
+                        responseFields(
+                                CommonSuccessResponseField.createCommonResponseFields()
+                        )
+                ));
+    }
+
+    @Test
+    @DisplayName("s3 프로필 이미지 삭제 실패")
+    void deleteProfileImageFail() throws Exception {
+        // given
+        doThrow(new RestApiException(S3ErrorCode.DELETE_IMAGE_ERROR))
+                .when(s3Controller).deleteProfileImage(any());
+
+        // When and Then
+        mockMvc.perform(delete("/api/profile")
+                        .header("Authorization", GIVEN_ACCESS_TOKEN)
+                        .contentType(MediaType.APPLICATION_JSON)
+                )
+                .andExpect(status().isBadRequest())
+                .andDo(document("profile/delete-fail",
+                        responseFields(
+                                CommonErrorResponseField.createCommonErrorResponseFields()
+                        )
+                ));
+    }
 }
