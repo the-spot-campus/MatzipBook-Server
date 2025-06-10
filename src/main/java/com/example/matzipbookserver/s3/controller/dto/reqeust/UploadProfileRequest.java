10eb05887c22fd588008dc427f512f6cbcb72f2e
taeyeongKims <xodud8530@gmail.com>
2025-06-10 14:52:55 +0900

    #19(UploadProfileRequest) validation 어노테이션 추가

--- src/main/java/com/example/matzipbookserver/s3/controller/dto/reqeust/UploadProfileRequest.java
+++ src/main/java/com/example/matzipbookserver/s3/controller/dto/reqeust/UploadProfileRequest.java
@@ -1,10 +1,14 @@
 package com.example.matzipbookserver.s3.controller.dto.reqeust;
 
+import com.example.matzipbookserver.global.validation.NotEmptyFile;
 import com.example.matzipbookserver.member.domain.Member;
+import jakarta.validation.constraints.NotNull;
 import org.springframework.web.multipart.MultipartFile;
 
 public record UploadProfileRequest(
+        @NotNull(message = "사용자가 존재해야 합니다.")
         Member member,
+        @NotEmptyFile
         MultipartFile profileImage
 ) {
 }
