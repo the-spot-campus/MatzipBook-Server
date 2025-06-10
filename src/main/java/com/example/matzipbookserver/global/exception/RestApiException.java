d840bcbf87cf13864cd50982a9189191681e7578
taeyeongKims <xodud8530@gmail.com>
2025-06-10 15:44:07 +0900

    #19(RestApiException) errors 필드 삭제, 기존과 동일

--- src/main/java/com/example/matzipbookserver/global/exception/RestApiException.java
+++ src/main/java/com/example/matzipbookserver/global/exception/RestApiException.java
@@ -3,25 +3,17 @@ package com.example.matzipbookserver.global.exception;
 import com.example.matzipbookserver.global.response.error.ErrorCode;
 import lombok.Getter;
 
-import java.util.List;
-
 @Getter
 public class RestApiException extends RuntimeException {
 
     private final ErrorCode errorCode;
-    private final List<String> errors;
 
     public RestApiException(ErrorCode errorCode) {
-        this(errorCode, null, null);
+        this.errorCode = errorCode;
     }
 
     public RestApiException(ErrorCode errorCode, String detailMessage) {
-        this(errorCode, detailMessage, null);
-    }
-
-    public RestApiException(ErrorCode errorCode, String detailMessage, List<String> errors) {
         super(detailMessage);
         this.errorCode = errorCode;
-        this.errors = errors;
     }
 }
